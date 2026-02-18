cf3ea84e0be05eea281249a49f65e769b3d1c020
Siyabonga Majola <siyabongamavundla@yahoo.com>
2026-02-18 10:31:46 +0200

    Downgrade plugin version from 1.0.1 to 1.0.0

--- woo-odoo-bridge.php
+++ woo-odoo-bridge.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Woo Odoo Bridge
  * Description: Bi-directional WooCommerce ↔ Odoo integration.
- * Version: 1.0.1
+ * Version: 1.0.0
  * Author: Siyabonga Majola
  * 
  * GitHub Plugin URI: https://github.com/djsiya27/woo-odoo-bridge
